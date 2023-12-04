a686b455340d37efb89464f81cb6a602ed8e7576
zhengkaifu <1119027725@qq.com>
2023-12-04 10:21:05 +0800

    test commit

---
+++ sql/PlantDevision.sql
@@ -0,0 +1,14 @@
+# INSERT INTO PlantDevision (FamilyName, GenusName, SpeciesName, AnotherName, DistributionAreaID, GrowingEnvironment, Creator, CreationTime, UpdateTime)
+# VALUES
+#     ('白花丹科', '补血草属', '二色补血草', '蝇子草', RAND() % 236 + 1, '', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+#     ('白花丹科', '补血草属', '珊瑚补血草', '无叶补血草', RAND() % 236 + 1, '', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+#     ('报春花科', '琉璃繁缕属', '琉璃繁缕', '海绿', RAND() % 236 + 1, '', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+
+INSERT INTO PlantDevision (FamilyName, GenusName, SpeciesName, AnotherName, DistributionAreaID, GrowingEnvironment, Creator, CreationTime, UpdateTime)
+VALUES
+    ('报春花科', '珍珠菜属', '长蕊珍珠菜', '白豆瓣菜', RAND() % 236 + 1, '', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('唇形科', '益母草属', '錾菜', '白花益母草', RAND() % 236 + 1, '', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('大风子科', '山桂花属', '山桂花', '木本远志', RAND() % 236 + 1, '', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('大戟科', '大戟属', '狼毒', '续毒', RAND() % 236 + 1, '', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('大戟科', '大戟属', '大戟', '京大戟', RAND() % 236 + 1, '', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
